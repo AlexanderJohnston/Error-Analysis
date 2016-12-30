7f21f3370deef92552abbb53ab63b0323bf63fb5
AlexanderJohnston <alexanderj.roaming@gmail.com>
2016-12-30 13:32:13 -0500

    Initialized project with a few basic buttons.

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Error Analysis.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ErrorAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textFilePath = new System.Windows.Forms.TextBox();
+            this.labelFilePath = new System.Windows.Forms.Label();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.buttonAnalyze = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textFilePath
+            // 
+            this.textFilePath.Location = new System.Drawing.Point(12, 36);
+            this.textFilePath.Name = "textFilePath";
+            this.textFilePath.Size = new System.Drawing.Size(499, 20);
+            this.textFilePath.TabIndex = 0;
+            // 
+            // labelFilePath
+            // 
+            this.labelFilePath.AutoSize = true;
+            this.labelFilePath.Location = new System.Drawing.Point(12, 20);
+            this.labelFilePath.Name = "labelFilePath";
+            this.labelFilePath.Size = new System.Drawing.Size(49, 13);
+            this.labelFilePath.TabIndex = 1;
+            this.labelFilePath.Text = "Data File";
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Location = new System.Drawing.Point(333, 63);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(86, 22);
+            this.buttonSelectFile.TabIndex = 2;
+            this.buttonSelectFile.Text = "Select File";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonAnalyze
+            // 
+            this.buttonAnalyze.Location = new System.Drawing.Point(425, 63);
+            this.buttonAnalyze.Name = "buttonAnalyze";
+            this.buttonAnalyze.Size = new System.Drawing.Size(86, 22);
+            this.buttonAnalyze.TabIndex = 3;
+            this.buttonAnalyze.Text = "Analyze";
+            this.buttonAnalyze.UseVisualStyleBackColor = true;
+            // 
+            // ErrorAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 525);
+            this.Controls.Add(this.buttonAnalyze);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.labelFilePath);
+            this.Controls.Add(this.textFilePath);
+            this.Name = "ErrorAnalysis";
+            this.Text = "Error Analysis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textFilePath;
+        private System.Windows.Forms.Label labelFilePath;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Button buttonAnalyze;
+    }
+}
+
